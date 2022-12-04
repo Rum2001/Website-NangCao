ff6b9a36bdfb929e6a850bd94647c0b3d125e1f1
Rum2001 <61676156+Rum2001@users.noreply.github.com>
2022-12-04 13:11:34 +0700

    commit

---
+++ english-app/resources/views/contact.php
@@ -0,0 +1,270 @@
+
+
+
+<div class="contact">
+  <div class="contact__map">
+    <iframe
+      src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3919.2534759067485!2d106.6933943141166!3d10.79188816186076!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x317528cb0b0f1723%3A0x397a5326e4c7cc88!2zMTk3IEzDqiBWxINuIER1eeG7h3QsIFTDom4gxJDhu4tuaCwgUXXhuq1uIDEsIFRow6BuaCBwaOG7kSBI4buTIENow60gTWluaCwgVmnhu4d0IE5hbQ!5e0!3m2!1svi!2s!4v1667265294730!5m2!1svi!2s"
+      width="100%"
+      height="450"
+      style="border: 0"
+      allowfullscreen=""
+      loading="lazy"
+      referrerpolicy="no-referrer-when-downgrade"
+    >
+    </iframe>
+  </div>
+  <div class="contact__container container-fluid" style="max-width: 1320px">
+    <div class="row">
+      <div class="contact__address col-6">
+        <div class="contact__header">
+          <h2>Keep In Touch With Us</h2>
+          <p class="w-75 text-muted">
+            Neque convallis a cras semper auctor. Libero id faucibus nisl
+            tincidunt egetnvallis.
+          </p>
+        </div>
+        <div class="contact__content">
+          <ul class="p-0 m-0 text-muted">
+
+            <li>
+              <span>
+                <img alt=''
+                  src="https://demoapus1.com/educrat/learnpress/wp-content/uploads/2022/07/pin.svg"
+                  style="width: 30px; height: 30px"/>
+                </span>
+              <p class="ms-3 m-0" style="width: 40%">
+                328 Queensberry Street, North Melbourne VIC 3051, Australia.
+              </p>
+            </li>
+
+            <li>
+              <span>
+                <img alt=''
+                  src="https://demoapus1.com/educrat/learnpress/wp-content/uploads/2022/07/phone.svg"
+                  style="width: 30px; height: 30px"/>
+            </span>
+              <p class="ms-3 m-0" style="width: 40%">+(1) 123 456 7890</p>
+            </li>
+
+            <li>
+              <span>
+                <img alt=''
+                  src="https://demoapus1.com/educrat/learnpress/wp-content/uploads/2022/07/mail.svg"
+                  style="width: 30px; height: 30px"/>
+                </span>
+              <p class="ms-3 m-0" style="width: 40%">hi@educrat.com</p>
+            </li>
+            
+          </ul>
+        </div>
+      </div>
+
+      <div class="contact__messages col-6">
+        <div class='contact__header'>
+          <h2>Send a Message</h2>
+          <p class="w-75 text-muted">
+            Neque convallis a cras semper auctor. Libero id faucibus nisl
+            tincidunt egetnvallis.
+          </p>
+        </div>
+
+        <div class="contact__input">
+          <div class="messages__top row">
+            
+          <div class="col-6">
+              <label>Name</label>
+              <input
+                class="w-100"
+                placeholder="Ali"
+                type="text"
+              />
+            </div>
+
+            <div class="col-6">
+              <label>Name</label>
+              <input
+                class="w-100"
+                placeholder="example@gmail.com"
+                type="text"
+              />
+            </div>
+
+          </div>
+          
+          <div class="messages__bot">
+            <label>Messages</label>
+            <textarea
+              class="w-100 d-block"
+            ></textarea>
+          </div>
+        </div>
+        <a class="ahover d-block"
+          ><button
+            class="px-4 py-3 hover"
+            style="
+              background-color: #6440fb;
+              color: white;
+              border-radius: 7px;
+              border: none;
+            "
+          >
+            Send Messages
+          </button></a
+        >
+      </div>
+    </div>
+  </div>
+</div>
+
+
+
+
+<div class="questions">
+  <div class="questions__title container text-center">
+    <h2 class="">Frequently Asked Questions.</h2>
+    <p class='text-muted'>Ut enim ad minim veniam, quis nostrud exercitation ullamco</p>
+  </div>
+
+  <div class="questions__container container d-grid" style="width: 850px">
+    <div class="questions__group">
+      <div class="questions__item">
+        <div class="questions__header">
+          <i class="d-flex"
+            ><img alt=''
+              src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/Approve_icon.svg/2048px-Approve_icon.svg.png"
+              style="height: 20px; width: 20px"
+          /></i>
+          <h3>Do you offer discounts for students?</h3>
+        </div>
+        <div class="questions__content">
+          <div class="questions__description">
+            <p class="m-0">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco.
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <div class="questions__item content-open">
+        <div class="questions__header">
+          <i class="d-flex"
+            ><img alt=''
+              src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/Approve_icon.svg/2048px-Approve_icon.svg.png"
+              style="height: 20px; width: 20px"
+          /></i>
+          <h3>Do you offer special pricing for big teams?</h3>
+        </div>
+        <div class="questions__content">
+          <div class="questions__description">
+            <p class="m-0">
+              LSed ut perspiciatis unde omnis iste natus error sit voluptatem
+              accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
+              quae ab illo inventore veritatis et quasi architecto beatae vitae
+              dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit
+              aspernatur aut odit aut fugit, sed quia consequuntur magni dolores
+              eos qui ratione voluptatem sequi nesciunt.
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <div class="questions__item">
+        <div class="questions__header">
+          <i class="d-flex"
+            ><img alt=''
+              src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/Approve_icon.svg/2048px-Approve_icon.svg.png"
+              style="height: 20px; width: 20px"
+          /></i>
+          <h3>What is the past experience of your teachers?</h3>
+        </div>
+        <div class="questions__content">
+          <div class="questions__description">
+            <p class="m-0">
+              At vero eos et accusamus et iusto odio dignissimos ducimus qui
+              blanditiis praesentium voluptatum deleniti atque corrupti quos
+              dolores et quas molestias excepturi sint occaecati cupiditate non
+              provident, similique sunt in culpa qui officia deserunt mollitia
+              animi, id est laborum et dolorum fuga. Et harum quidem rerum
+              facilis est et expedita distinctio.
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <div class="questions__item">
+        <div class="questions__header">
+          <i class="d-flex"
+            ><img alt=''
+              src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/Approve_icon.svg/2048px-Approve_icon.svg.png"
+              style="height: 20px; width: 20px"
+          /></i>
+          <h3>What is the course refund policy?</h3>
+        </div>
+        <div class="questions__content">
+          <div class="questions__description">
+            <p class="m-0">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco.
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <div class="questions__item">
+        <div class="questions__header">
+          <i class="d-flex"
+            ><img alt=''
+              src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/Approve_icon.svg/2048px-Approve_icon.svg.png"
+              style="height: 20px; width: 20px"
+          /></i>
+          <h3>Do you offer discounts for non-profits?</h3>
+        </div>
+        <div class="questions__content">
+          <div class="questions__description">
+            <p class="m-0">
+              At vero eos et accusamus et iusto odio dignissimos ducimus qui
+              blanditiis praesentium voluptatum deleniti atque corrupti quos
+              dolores et quas molestias excepturi sint occaecati cupiditate non
+              provident, similique sunt in culpa qui officia deserunt mollitia
+              animi, id est laborum et dolorum fuga. Et harum quidem rerum
+              facilis est et expedita distinctio.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+<script>
+  const questItems = document.querySelectorAll(".questions__item");
+
+questItems.forEach((element, index) => {
+  element.addEventListener("click", () => {
+    const checkItemOpen = document.querySelector(".content-open");
+
+    toggleItem(element);
+
+    if (checkItemOpen && checkItemOpen !== element) {
+      toggleItem(checkItemOpen);
+    }
+  });
+});
+
+const toggleItem = (element) => {
+  const questContent = element.querySelector(".questions__content");
+
+  if (element.classList.contains("content-open")) {
+    questContent.removeAttribute("style");
+    element.classList.remove("content-open");
+  } else {
+    questContent.style.height = questContent.scrollHeight + "px";
+    element.classList.add("content-open");
+  }
+};
+
+</script>
